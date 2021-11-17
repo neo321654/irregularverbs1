cec1548e5437f4a63b7071139e5f02805fdac953
neo321654 <neo321654@rambler.ru>
2021-11-17 20:36:35 +0300

    add shared pref

--- lib/src/settings/settings_controller.dart
+++ lib/src/settings/settings_controller.dart
@@ -25,7 +25,7 @@ class SettingsController with ChangeNotifier {
   /// local database or the internet. The controller only knows it can load the
   /// settings from the service.
   Future<void> loadSettings() async {
-    _themeMode = await _settingsService.themeMode();
+    _themeMode = await _settingsService.getThemeMode();
 
     // Important! Inform listeners a change has occurred.
     notifyListeners();
--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// A service that stores and retrieves user settings.
 ///
@@ -6,13 +7,38 @@ import 'package:flutter/material.dart';
 /// persist the user settings locally, use the shared_preferences package. If
 /// you'd like to store settings on a web server, use the http package.
 class SettingsService {
-  
-  /// Loads the User's preferred ThemeMode from local or remote storage.
-  Future<ThemeMode> themeMode() async => ThemeMode.system;
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  late SharedPreferences prefs;
+
+  Future<ThemeMode> getThemeMode() async {
+    prefs = await _prefs;
+    switch(prefs.get('ThemeMode')){
+      case "dark":
+        return ThemeMode.dark;
+      case "light":
+        return ThemeMode.light;
+    }
+    return ThemeMode.dark;
+  }
 
   /// Persists the user's preferred ThemeMode to local or remote storage.
   Future<void> updateThemeMode(ThemeMode theme) async {
+    //final SharedPreferences prefs = await _prefs;
+    String newThemeStr;
 
+    switch (theme) {
+      case ThemeMode.system:
+        newThemeStr = "dark";
+        break;
+      case ThemeMode.light:
+        newThemeStr = "light";
+        break;
+      case ThemeMode.dark:
+        newThemeStr = "dark";
+        break;
+    }
+
+    prefs.setString("ThemeMode", newThemeStr);
     // Use the shared_preferences package to persist settings locally or the
     // http package to persist settings over the network.
   }
